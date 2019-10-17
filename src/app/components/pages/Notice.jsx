ca5e698857d14c0b5135431b14643b11232431d0
Jacob <wnsl5684@gmail.com>
2019-10-17 13:46:22 +0900

    get data from api

--- src/app/components/pages/Notice.jsx
+++ src/app/components/pages/Notice.jsx
@@ -1,8 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router';
 import tt from 'counterpart';
+import axios from 'axios';
+
+async function callApi(url, params) {
+    return await axios({
+        url,
+        method: 'GET',
+        params,
+    })
+        .then(response => {
+            return response.data;
+        })
+        .catch(err => {
+            console.error(`Could not fetch data, url: ${url}`);
+            return {};
+        });
+}
 
 class Notice extends React.Component {
+    constructor() {
+        super();
+        this.state = {};
+        console.log('Notice constructor');
+
+        callApi('https://passionbull.github.io/steemcoinpan/sctpins').then(
+            results => {
+                console.log(results);
+            }
+        );
+    }
+
+    componentDidUpdate(prevProps) {}
+
     render() {
         return (
             <div className="row">
